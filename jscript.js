4fd2bdce3c3f664b116bb83b9e820b3d6e5ca5c9
Shivam Tarone <shivamtarone07@gmail.com>
2020-03-05 22:38:04 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,13 @@
+
+
+  	 $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll < 300){
+            $('.fixed-top').css('background', 'rgba(23,67,88,0.5)');
+        }
+    });
+
+
+
+
+    
